58086cf43a34a62f0a18329929327f2df96c75c0
Adnan <adnan.lanewala@sciex.com>
2020-08-12 10:38:43 -0400

    Added keras model file, requirements file

---
+++ dev/keras_models.py
@@ -0,0 +1,26 @@
+import numpy as np
+import tensorflow as tf
+
+def VGG_model(image_w, image_h, l1=0, l2=0):
+    model = tf.keras.applications.VGG16(weights="imagenet", include_top=False, input_shape=(image_w, image_h, 3))
+    print(model.summary())
+    for layer in model.layers[0:19]:
+        layer.trainable = False
+
+    if l1 > 0:
+        regularizer = tf.keras.regularizers.l1(l1)
+    else:
+        regularizer = tf.keras.regularizers.l2(l2)
+
+    new_model = tf.keras.models.Sequential()
+    new_model.add(model)
+    new_model.add(tf.keras.layers.Flatten(name='flatten'))
+    new_model.add(tf.keras.layers.Dense(256, activation='softmax', name='new_fc1', kernel_regularizer=regularizer))
+    #new_model.add(tf.keras.layers.Dense(5, activation='softmax', name='new_predictions'))
+
+    # new_model.summary()
+    new_model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=["accuracy"])
+    print(new_model.summary())
+    return new_model
+
+
--- dev/main.py
+++ dev/main.py
@@ -1,7 +1,6 @@
-
-
-
+from keras_models import *
 
 
 if __name__ == '__main__':
+    model = VGG_model(150,150)
     print('Hello world')
