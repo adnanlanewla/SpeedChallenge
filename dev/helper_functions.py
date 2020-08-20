93d6371fd81f143569fea735a4755febe9217427
Hasnain Lanewala <hlanewala@gmail.com>
2020-08-20 11:26:47 -0700

    Cleaned up code

--- dev/helper_functions.py
+++ dev/helper_functions.py
@@ -1,12 +1,10 @@
 import cv2
 import os
 import numpy as np
-import pandas as pd
 
 def frame_extractor():
     #Create Image Folder
-    os.mkdir('../data/Images')
-
+    os.mkdir('../data/Local_Data/Images')
     filename = '../data/train.mp4'
     #Get video object
     captured_video = cv2.VideoCapture(filename)
@@ -15,13 +13,17 @@ def frame_extractor():
     success, image = captured_video.read()
     frame_number = 1
     fps = 20
+    f = open('../data/train.txt', 'r')
 
     while success:
         second = (frame_number - 1) // fps
         minute = second // 60
         frame_number_within_one_second = frame_number % fps + fps * int(not bool(frame_number % fps))
+        # read the speed of each image from train.txt and add it to the filename after an '_'
+        speed = f.readline()
+        speed = speed.rsplit('\n')[0]
         # name of each image
-        image_name = f'../data/Images/frame_{minute}_{second%60}_{frame_number_within_one_second}.jpg'
+        image_name = f'../data/Local_Data/Images/frame_{minute}_{second%60}_{frame_number_within_one_second}_{speed}.jpg'
         # save the image at 100% quality
         cv2.imwrite(image_name, image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
         # Keep running the command below until there are no more frames left
@@ -32,74 +34,7 @@ def frame_extractor():
 
         frame_number += 1
 
-
-def file_renamer():
-    # find the directory where the files are located using the relative location of the current script
-    directory = '../data/Images'
-
-    # Change the current python directory to where the files are located
-    os.chdir(directory)
-
-    # Get absolute path to the files
-    directory = os.getcwd()
-
-    # Get all the filenames
-    filenames = os.listdir(directory)
-
-    # sorted(mylist, key=WhatToSortBy)
-    # https://stackoverflow.com/questions/8966538/syntax-behind-sortedkey-lambda
-    filenames.sort(key=lambda x: os.path.getmtime(os.path.join(directory, x)))
-
-    # open the notepad file
-    f = open('../train.txt', 'r')
-
-    for file in filenames:
-        # read the speed in each line and append it to the filename after an '_'
-        speed = f.readline()
-        new_name = file.rsplit('.')[0] + '_' + speed.rsplit('\n')[0] + '.jpg'
-        os.rename(file, new_name)
-
     f.close()
-    # Change the current python directory to where this script file is
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
-
-def image_ndim_reshaper():
-    # find the directory where the files are located using the relative location of the current script
-    directory = '../data/Local Data/Images'
-
-    # Change the current python directory to where the files are located
-    os.chdir(directory)
-
-    # Get absolute path to the files
-    directory = os.getcwd()
-
-    # Get all the filenames
-    filenames = os.listdir(directory)
-
-    # sorted(mylist, key=WhatToSortBy)
-    # https://stackoverflow.com/questions/8966538/syntax-behind-sortedkey-lambda
-    filenames.sort(key=lambda x: os.path.getmtime(os.path.join(directory, x)))
-    array_col_size = cv2.imread(filenames[0])
-    array_col_size = array_col_size.shape[0] * array_col_size.shape[1] * array_col_size.shape[2]
-    training_set = np.empty((len(filenames)-1, array_col_size), dtype='uint8')
-    fps = 20
-
-
-    for i in range(len(filenames)-1):
-        A = cv2.imread(filenames[i])
-        A = np.ravel(A)
-        B = cv2.imread(filenames[i+1])
-        B = np.ravel(B)
-        C = A - B
-        training_set[i] = C
-        if not bool(i % (fps * 60)):
-            print(f'{i //(fps * 60)} minute(s) of video processed')
-
-    training_set = training_set.T
-    np.savetxt('../Frame_Differences.csv', training_set, delimiter=',', fmt='%u')
-    # pd.DataFrame(training_set.T).to_csv('../Frame_Differences_0-2.csv', header=False, index=False)
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 def speed_difference():
     path = '../data/train.txt'
@@ -107,10 +42,61 @@ def speed_difference():
     data = np.diff(data)
     np.savetxt('../data/Speed_Differences.txt', data, fmt='%f')
 
+def image_cropper():
+    # Create Image Folder
+    os.mkdir('../data/Local_Data/Cropped_Images')
+    # find the directory where the files are located using the relative location of the current script
+    directory = '../data/Local_Data/Images'
+    # Change the current python directory to where the files are located
+    os.chdir(directory)
+    # Get absolute path to the files
+    directory = os.getcwd()
+    # Get all the filenames
+    filenames = os.listdir(directory)
+    # sort the array based on the files' creation date
+    filenames.sort(key=lambda x: os.path.getmtime(os.path.join(directory, x)))
+    array_col_size = cv2.imread(filenames[0])
+
+    for i in range(len(filenames)):
+        A = cv2.imread(filenames[i])
+        cropped_A = A[20:360, :]
+        image_name = f'../Cropped_Images/' + filenames[i]
+        cv2.imwrite(image_name, cropped_A, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+def image_to_RGB(start_image_number = 1, num_of_images_per_file = 3600):
+    # find the directory where the files are located using the relative location of the current script
+    directory = '../data/Local_Data/Cropped_Images'
+    # Change the current python directory to where the files are located
+    os.chdir(directory)
+    # Get absolute path to the files
+    directory = os.getcwd()
+    # Get all the filenames
+    filenames = os.listdir(directory)
+    # sort the array based on the files' creation date
+    filenames.sort(key=lambda x: os.path.getmtime(os.path.join(directory, x)))
+    # read one file to extract its height, width and channels using the shape attribute
+    array_col_size = cv2.imread(filenames[0])
+    #examples is variable used to determine the number of images to be stored in one file. Its
+    #provided as a function argument
+    examples = num_of_images_per_file
+    #initialize numpy array which will contain rgb data for one output file. One
+    #output file contains data for a number of images specified by the 'examples' variable above
+    training_set = np.empty((examples, array_col_size.shape[0], array_col_size.shape[1], array_col_size.shape[2]), dtype=np.uint8)
+    # frame per second variable for housekeeping purposes in the for-loop below
+    fps = 20
+    #Loop end calculator
+    loop_end = min(start_image_number + examples - 1, len(filenames))
+
+    for i in range(start_image_number-1, loop_end):
+        training_set[i] = cv2.imread(filenames[i])
+        if not bool(i % (fps * 60)):
+            print(f'{i //(fps * 60)} minute(s) of video processed')
+
+    np.savez_compressed(f'../../Image_RGB_Values_{start_image_number}_{loop_end}', a = training_set)
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
 if __name__ == '__main__':
-    filename1 = '../data/Local Data/Images/frame_0_0_1_28.105569.jpg'
-    filename2 = '../data/Local Data/Images/frame_0_2_15_27.266277.jpg'
-    A = cv2.imread(filename1)
-    B = cv2.imread(filename2)
-    C = A + B
-    cv2.imwrite('../data/Local Data/test2.jpg', C)
+    None
+
