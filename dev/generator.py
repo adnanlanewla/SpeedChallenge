5a3ffd3aea4d5621016a3fe4ab0e3a7c9ba5151a
Adnan <adnan.lanewala@sciex.com>
2020-08-27 17:54:33 -0400

    Updated comments for generator.py

--- dev/generator.py
+++ dev/generator.py
@@ -24,6 +24,7 @@ class My_Custom_Generator(tf.keras.utils.Sequence):
     def __len__(self):
         return (np.ceil(len(self.image_filenames) / float(self.batch_size))).astype(np.int)
 
+    # This methods gets the size of the batch for X and Y
     def __getitem__(self, idx):
         batch_x = self.image_filenames[idx * self.batch_size: (idx + 1) * self.batch_size]
         batch_y = self.labels[idx * self.batch_size: (idx + 1) * self.batch_size]
